b29286f9bdf517bf5cbc47d6b7eb0165b6ea5098
Tony Hsu <tonyc.t.hsu@gmail.com>
2022-12-02 15:19:52 +0100

    https://github.com/rspec/rspec-rails/issues/1353#issuecomment-93173691

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -272,3 +272,22 @@ end
 
 # Helper matchers
 RSpec::Matchers.define_negated_matcher :not_be, :be
+
+puts "rspec pid: #{Process.pid}"
+
+trap 'USR1' do
+  threads = Thread.list
+
+  puts
+  puts "=" * 80
+  puts "Received USR1 signal; printing all #{threads.count} thread backtraces."
+
+  threads.each do |thr|
+    description = thr == Thread.main ? "Main thread" : thr.inspect
+    puts
+    puts "#{description} backtrace: "
+    puts thr.backtrace.join("\n")
+  end
+
+  puts "=" * 80
+end
