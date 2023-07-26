d2eddd03f35242fd8a8e323527b38886dcd8596b
Marco Costa <marco.costa@datadoghq.com>
2023-07-26 10:33:16 -0700

    wip

--- lib/datadog/opentelemetry/api/trace/span.rb
+++ lib/datadog/opentelemetry/api/trace/span.rb
@@ -7,6 +7,21 @@ module Datadog
       module Span
         attr_accessor :datadog_trace, :datadog_span
 
+        def set_attribute(key, value)
+          @mutex.synchronize do
+            if @ended
+              OpenTelemetry.logger.warn('Calling set_attribute on an ended Span.')
+            else
+              @attributes ||= {}
+              @attributes[key] = value
+              trim_span_attributes(@attributes)
+              @total_recorded_attributes += 1
+            end
+          end
+          self
+        end
+        alias []= set_attribute
+
         ::OpenTelemetry::Trace::Span.prepend(self)
       end
     end
